c7e7e9f456977d46975de9bf0d272332b3d8fa28
Devang Nathwani <devangnathwani@Devangs-MacBook-Air.local>
2017-06-09 21:59:00 +0530

    Initial Commit

---
+++ Quizee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quizee
+//
+//  Created by Devang Nathwani on 09/06/17.
+//  Copyright © 2017 DND. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ QuizeeTests/QuizeeTests.swift
@@ -0,0 +1,36 @@
+//
+//  QuizeeTests.swift
+//  QuizeeTests
+//
+//  Created by Devang Nathwani on 09/06/17.
+//  Copyright © 2017 DND. All rights reserved.
+//
+
+import XCTest
+@testable import Quizee
+
+class QuizeeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
